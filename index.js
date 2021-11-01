c8c66df58644528515573d9876c26a6d061c7a3e
= <eduardo.plis@hotmail.com>
2021-11-01 14:35:04 -0300

    adicionando link no push [19689573]

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 var postComment = require('./src/build/artiaApi/Comments/postComment.js');
 const event = github.context.eventName;
+// // Get the JSON webhook payload for the event that triggered the workflow//
 const payload            = JSON.stringify(github.context.payload, undefined, 2) 
 const objPayload         = JSON.parse(payload)
 const organizationId     = core.getInput('organizationId') //OrganizationId informado no main.yml do workflow
 const accountId          = core.getInput('accountId') //AccountId informado no main.yml do workflow
 
 try {
-// // Get the JSON webhook payload for the event that triggered the workflow//
+
 
 
 switch (event){
   case 'push':
-    var pushContent          = objPayload.commits[0].message;
-    const pushActivityId     = pushContent.split('[').pop().split(']')[0]; 
-    pushContent              = pushContent.replace('['+pushActivityId.toString+']',"");
+    
+    const pushActivityId     = objPayload.commits[0].message.split('[').pop().split(']')[0]; 
+    var pushContent          = objPayload.commits[0].message.replace('['+pushActivityId.toString+']',"");
+    pushContent              += " | link da alteração no git:" + objPayload.compare
     var newComment           = postComment(organizationId, accountId, pushActivityId, pushContent);
+
   break;
 
   case 'pull_request':
