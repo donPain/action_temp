5777aeb63d7fd1e1807793badcfa7fa2f011e567
= <eduardo.plis@hotmail.com>
2021-11-01 13:48:38 -0300

    testando o context

--- index.js
+++ index.js
@@ -7,21 +7,25 @@ var postComment = require('./src/build/artiaApi/Comments/postComment.js');
 
 try {
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2) 
-  const objPayload = JSON.parse(payload)
-  // const pullRequest = objPayload.pull_request;
+// // Get the JSON webhook payload for the event that triggered the workflow//
+// const payload     = JSON.stringify(github.context.payload, undefined, 2) 
+// const objPayload  = JSON.parse(payload)
+// const pullRequest = objPayload.pull_request;
+// //-------------------------------------------------------------------------//
+// const organizationId = core.getInput('organizationId') //OrganizationId informado no main.yml do workflow
+// const accountId      = core.getInput('accountId') //AccountId informado no main.yml do workflow
+// const activityId     = pullRequest.title.split('[').pop().split(']')[0]; // returns ActivityId
+// const content        = pullRequest.body.toString();
 
-const organizationId = core.getInput('organizationId') //OrganizationId informado no main.yml do workflow
-const accountId  = core.getInput('accountId') //AccountId informado no main.yml do workflow
-const activityId = 19689573//pullRequest.title.split('[').pop().split(']')[0]; // returns ActivityId
-const content    = objPayload.commits[0].message;
+console.log(github.context.eventName);
 
-var newComment = postComment(organizationId, accountId, activityId, content);
+// var newComment = postComment(organizationId, accountId, activityId, content);
 
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
 
 
 
