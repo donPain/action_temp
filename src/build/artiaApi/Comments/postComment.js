2c66d8591a842fff679c52c7aad749147c0b9f0f
= <eduardo.plis@hotmail.com>
2021-11-01 10:15:12 -0300

    Tag 1.8

--- src/build/artiaApi/Comments/postComment.js
+++ src/build/artiaApi/Comments/postComment.js
@@ -1,29 +1,21 @@
+"use strict";
 var unirest = require("unirest");
 var asyncGetToken = require("../Authorization/getToken");
-
 // // const content: String = "Comentário da api";
 // const organizationId: Number = 111402;
 // const accountId: Number = 3757321;
 // const activityId: Number = 19689573;
-
 //Parametros la do core do action {organizationId, accountId}
 //Parametros informados no commit através de t:{activityId} | tudo que estiver dentro do comentário irá para tarefa.
-
-module.exports = async function createComment(
-  organizationId: number,
-  accountId: number,
-  activityId: number,
-  content: string
-) {
-  var newToken = await asyncGetToken();
-  var req = unirest("POST", "https://app.artia.com/graphql")
-    .headers({
-      OrganizationId: organizationId.toString(),
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + newToken,
+module.exports = async function createComment(organizationId, accountId, activityId, content) {
+    var newToken = await asyncGetToken();
+    var req = unirest("POST", "https://app.artia.com/graphql")
+        .headers({
+        OrganizationId: organizationId.toString(),
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + newToken,
     })
-    .send(
-      JSON.stringify({
+        .send(JSON.stringify({
         query: `mutation{
       createComment(
           accountId: ${accountId}, #obrigatório
@@ -55,10 +47,10 @@ module.exports = async function createComment(
       }
   }`,
         variables: {},
-      })
-    )
-    .end(function (res: { error: string | undefined; raw_body: any }) {
-      if (res.error) throw new Error(res.error);
-      console.log(res.raw_body);
+    }))
+        .end(function (res) {
+        if (res.error)
+            throw new Error(res.error);
+        console.log(res.raw_body);
     });
 };
